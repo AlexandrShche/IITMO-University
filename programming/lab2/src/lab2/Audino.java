c5b7effe40abffcda823941c03fd0903f3323547
Neegroom <56590486+Neegroom@users.noreply.github.com>
2020-10-13 00:41:42 +0300

    lab2 update

--- programming/lab2/src/lab2/Audino.java
+++ programming/lab2/src/lab2/Audino.java
@@ -1,12 +1,34 @@
-package lab2;
+import java.awt.Window.Type;
 
-import ru.ifmo.se.pokemon.Pokemon;
+import ru.ifmo.se.pokemon.*;
 
-public class Audino extends Pokemon {
+public class Audino  extends Pokemon {
 	public Audino(String name, int lvl ) {
 		super(name, lvl);
 		setStats(103, 60, 86, 60, 86, 50);
 		setType(Type.NORMAL);
-		setMove();
+		setMove(new HuperVoice(), new DreamEater(), new Growl(), new BabyDollEyes());
 	}
+}
+
+class HuperVoice extends SpecialMove {
+	public HuperVoice(){
+		super(Type.NORMAL, 90, 1.0);
+	}
+}
+
+class DreamEater extends SpecialMove{
+	public DreamEater(){
+		super(Type.PSYCHIC, 100, 1.0);
+	}
+}
+
+class Growl extends StatusMove{
+	public Growl(){
+		super(Type.NORMAL, 0, 1.0);
+
+	}
+
+	@Override
+	
 }
--- programming/lab2/src/lab2/lab2.java
+++ programming/lab2/src/lab2/lab2.java
@@ -7,9 +7,14 @@ public class lab2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		Audino a1 = new Audino("hui",);
-		System.out.println(a1.getHP());
+		Battle b = new Battle();
+		Audino p1 = new Audino("hui",10);
+		System.out.println(p1.getHP());
+
+		Pokemon p2 = new Pokemon("Хищник", 1);
+		b.addAlly(p1);
+		b.addFoe(p2);
+		b.go();
 	}
 
 }
