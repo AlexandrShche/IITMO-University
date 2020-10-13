7e15382d049baa70e1a14c5c6f110be34ef1b0ea
Neegroom <56590486+Neegroom@users.noreply.github.com>
2020-10-13 16:33:51 +0300

    lab2

--- programming/lab2/src/lab2/lab2.java
+++ programming/lab2/src/lab2/lab2.java
@@ -1,19 +1,23 @@
-package lab2;
-
 import ru.ifmo.se.pokemon.*;
-
 
 public class lab2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Battle b = new Battle();
-		Audino p1 = new Audino("hui",10);
-		System.out.println(p1.getHP());
+		Audino p1 = new Audino("Au",10);
+		Pumpkaboo p2 = new Pumpkaboo("Pu", 10);
+		Gourgeist p3 = new Gourgeist("Go", 10);
+		Igglybuff p4 = new Igglybuff("Ig", 10);
+		Jigglypuff p5 = new Jigglypuff("Ji", 11);
+		Wigglytuff p6 = new Wigglytuff("Wi", 9);
 
-		Pokemon p2 = new Pokemon("Хищник", 1);
 		b.addAlly(p1);
-		b.addFoe(p2);
+        b.addAlly(p2);
+		b.addAlly(p3);
+		b.addFoe(p4);
+		b.addFoe(p5);
+		b.addFoe(p6);
+
 		b.go();
 	}
 
