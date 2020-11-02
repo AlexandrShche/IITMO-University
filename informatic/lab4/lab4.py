5670bad33c3a76933737beb0e16b2d7ab0e25c0d
AlexanderShche <Neegroom@yandex.ru>
2020-11-02 09:07:28 +0300

    added tests

--- informatic/lab4/lab4.py
+++ informatic/lab4/lab4.py
@@ -1,12 +1,12 @@
 import re
 
-for i in range(1):
+for i in range(5):
     fileName = 'test' + str(i+1) +'.txt'    
     file = open(fileName, encoding="utf-8")    
     text = file.read()
-    pattern = re.compile((\s)([0,1]\d|2[0-3])(:[0-5]\d){1,2})
+    pattern = re.compile(r"(\s)([0,1]\d|2[0-3])(:[0-5]\d){1,2}")
     matches = pattern.finditer(text)
     for j in matches:
-        text = pattern.sub(matches[j], (TBD), text, j)
+        text = re.sub(pattern, " (TBD)", text)
     print('\n' + text + '\n')
     file.close()
