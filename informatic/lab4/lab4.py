ae4a92cd6f4f028029da6ffe37669d090895ff2b
AlexanderShche <Neegroom@yandex.ru>
2020-11-07 14:44:41 +0300

    lab4 informatic

--- informatic/lab4/lab4.py
+++ informatic/lab4/lab4.py
@@ -2,11 +2,11 @@ import re
 
 for i in range(5):
     fileName = 'test' + str(i+1) +'.txt'    
-    file = open(fileName, encoding="utf-8")    
+    file = open(fileName)    
     text = file.read()
     pattern = re.compile(r"(\s)([0,1]\d|2[0-3])(:[0-5]\d){1,2}")
     matches = pattern.finditer(text)
     for j in matches:
         text = re.sub(pattern, " (TBD)", text)
-    print('\n' + text + '\n')
+    print(text + '\n')
     file.close()
